893cc5be98d47ca1fa97abb0b846f18c2ba0011c
ProgramacaoP <paulalisboarp@gmail.com>
2025-03-25 23:13:32 +0000

    declaração de variaveis

--- TESTESUPERTRUNFO/testesupertrunfo.c
+++ TESTESUPERTRUNFO/testesupertrunfo.c
@@ -2,7 +2,16 @@
 
 int main() {
     printf("Cadastro de Cartas Super Trunfo\n");
-    
+
+    int Carta;
+    char Estado;
+    char odigodaCarta[4];
+    char Cidade[9];
+    int População;
+    float AreaemKM²;
+    float Pib;
+    int Numerodepontoturistico;
+
 
 
 }
