128ff47cdc507744a61df44444463fb8e37f1d90
ProgramacaoP <paulalisboarp@gmail.com>
2025-04-05 12:24:17 +0000

    inseeri variaveis, iniciando variaveis, lendo e imprimindo variaveis

--- teste/horadecodarintermediario.c
+++ teste/horadecodarintermediario.c
@@ -2,6 +2,22 @@
 
 int main() {
 
+    int nota1, nota2, nota3;
+    int media;
 
-    
-}
+    printf("*** Programa de Cálculo de Média ***\n");
+
+    printf("Digite a sua primeira nota:\n");
+    scanf("%d", &nota1);
+
+    printf("Digite a sua segunda nota:\n");
+    scanf("%d", &nota2);
+
+    printf("Digite a sua terceira nota:\n");
+    scanf("%d", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A Média é: %d\n", media);
+
+    return 0;    
